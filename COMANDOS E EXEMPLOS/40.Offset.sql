5c2142fb14fa435a562c4bb9dbad567a60daf6b6
Rodrigo Castro <rodricastroxmx@gmail.com>
2025-02-26 15:39:16 -0300

    Inserção mais casos de Offset, e início dos exercícios do módulo 14

--- COMANDOS E EXEMPLOS/40.Offset.sql
+++ COMANDOS E EXEMPLOS/40.Offset.sql
@@ -19,3 +19,50 @@ SELECT
 	LEAD(Faturamento_MM, 1, 0) OVER(ORDER BY Data_Fechamento) AS 'Lead' --Ordenou pela data, e deslocou os valores do Faturamento, uma linha a cima
 FROM
 	Resultado
+
+	--Cálculo MoM (Month over Month): Variação de um mês em relação à outro
+		-- (Valor mês atual) / (Valor mês anterior) - 1
+
+SELECT
+	Data_Fechamento,
+	Mes_Ano,
+	Faturamento_MM,
+	FORMAT((Faturamento_MM / LAG(Faturamento_MM, 1) OVER(ORDER BY Data_Fechamento)) - 1, '0.00%') AS '% MoM'
+FROM
+	Resultado
+
+	--Outra forma de resolver usando o terceiro argumento do LAG
+		--NULLIF: Caso o valor do primeiro seja diferente do segundo, retorna o primeiro, caso seja igual, retorna NULL
+
+SELECT
+	Data_Fechamento,
+	Mes_Ano,
+	Faturamento_MM,
+	FORMAT((Faturamento_MM / NULLIF(LAG(Faturamento_MM, 1, 0) OVER(ORDER BY Data_Fechamento), 0)) - 1, '0.00%') AS '% MoM'
+FROM
+	Resultado
+
+	--FIRST_VALUE: Retorna o primeiro valor da coluna especificada
+
+SELECT
+	Data_Fechamento,
+	Mes_Ano,
+	Faturamento_MM,
+	FIRST_VALUE(Faturamento_MM) OVER(ORDER BY Data_Fechamento ASC) AS 'Primeiro valor'
+FROM
+	Resultado
+ORDER BY
+	Data_Fechamento 
+
+	--LAST_VALUE: Retorna o último valor da coluna especificada
+		--Necessário especificar também as linhas, se não vai retornar o valor da linha atual
+
+SELECT
+	Data_Fechamento,
+	Mes_Ano,
+	Faturamento_MM,
+	LAST_VALUE(Faturamento_MM) OVER(ORDER BY Data_Fechamento ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS 'Último valor'
+FROM
+	Resultado
+ORDER BY
+	Data_Fechamento 
---
+++ EXERCICIOS/MODULO 14 - WINDOW FUNCTIONS.sql
@@ -0,0 +1,69 @@
+/*Para resolver os exercícios 1 a 4, crie uma View chamada vwProdutos, que contenha o 
+agrupamento das colunas BrandName, ColorName e os totais de quantidade vendida por 
+marca/cor e também o total de receita por marca/cor. */
+GO
+CREATE VIEW 
+	vwProdutos AS 
+		SELECT 
+			BrandName AS 'Marca', 
+			ColorName AS 'Cor', 
+			SUM(SalesQuantity) AS 'Quantidade_Vendida', 
+			ROUND(SUM(SalesAmount), 2) AS 'Receita_Total' 
+		FROM 
+			DimProduct 
+		INNER JOIN 
+			FactSales 
+		ON 
+			DimProduct.ProductKey = FactSales.ProductKey 
+		GROUP BY 
+			BrandName, 
+			ColorName
+
+--1. Utilize a View vwProdutos para criar uma coluna extra calculando a quantidade total vendida dos produtos.
+
+GO
+SELECT
+	*,
+	SUM(Quantidade_Vendida) OVER() AS 'Total Vendido'
+FROM
+	vwProdutos
+ORDER BY
+	Marca
+
+--2. Crie mais uma coluna na consulta anterior, incluindo o total de produtos vendidos para cada marca. 
+
+GO
+SELECT
+	*,
+	SUM(Quantidade_Vendida) OVER() AS 'Total Vendido',
+	SUM(Quantidade_Vendida) OVER(PARTITION BY Marca) AS 'Total Vendido por Marca'
+FROM
+	vwProdutos
+ORDER BY
+	Marca
+
+--3. Calcule o % de participação do total de vendas de produtos por marca.  
+
+GO
+SELECT
+	*,
+	SUM(Quantidade_Vendida) OVER() AS 'Total Vendido',
+	SUM(Quantidade_Vendida) OVER(PARTITION BY Marca) AS 'Total Vendido por Marca',
+	FORMAT(CAST(SUM(Quantidade_Vendida) OVER(PARTITION BY Marca) AS float) / CAST(SUM(Quantidade_Vendida) OVER() AS float), '0.00%') AS '% de participação por marca'
+FROM
+	vwProdutos
+ORDER BY
+	Marca
+
+--4. Crie uma consulta à View vwProdutos, selecionando as colunas Marca, Cor, Quantidade_Vendida e também criando uma coluna extra de Rank para descobrir a posição de cada Marca/Cor.
+--Obs: Sua consulta deve ser filtrada para que seja mostrada apenas a marca Contoso. 
+
+SELECT
+	Marca,
+	Cor,
+	Quantidade_Vendida,
+	RANK() OVER(ORDER BY Quantidade_Vendida DESC) AS 'Rank'
+FROM
+	vwProdutos
+WHERE
+	Marca = 'Contoso'
